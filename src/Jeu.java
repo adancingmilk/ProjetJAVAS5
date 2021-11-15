6d1e2f77aadc355076c771af4c5aa8aed13f4e2f
alachaud <antoine.lachaud91620@gmail.com>
2021-11-15 16:01:44 +0100

    Ajout des instructions d'execution dans Jeu.java

--- src/Jeu.java
+++ src/Jeu.java
@@ -1,5 +1,12 @@
 public class Jeu {
     public static void main(String[] args) {
-
+//        1. Afficher les 10 thèmes choisis
+//        2. Créer une liste de questions pour chaque thème
+//        3. Afficher toutes les questions d’un niveau n donné par thème
+//        4. Ajouter une question à la liste pour un thème donné
+//        5. Supprimer une question de numéro n de la liste pour un thème donné
+//        6. Créer le tableau de joueurs et afficher leurs états
+//        7. Lancer une partie du jeu avec 4 joueurs choisis en affichant toutes les étapes du déroulement du jeu
+//        8. Quitter le jeu
     }
 }
