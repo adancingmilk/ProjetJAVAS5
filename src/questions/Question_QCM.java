ca424928a13e2a8e2759067043cf155d2cc3c46f
Nyu_code <jacqueswu12@gmail.com>
2021-12-05 23:49:02 +0100

    Phase 1 + corrections des autres classes

--- src/questions/Question_QCM.java
+++ src/questions/Question_QCM.java
@@ -1,14 +1,25 @@
 package questions;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Question_QCM extends Question {
+    private String[] reponses;
 
     public Question_QCM(int d, String description, String type, int num, String[] reponses, String bonneRep) {
         super(d, description, type, num, bonneRep);
+        this.reponses = reponses;
+    }
+
+    public String[] getReponses() {
+        return reponses;
+    }
+
+    public void setReponses(String[] reponses) {
+        this.reponses = reponses;
     }
 
     @Override
     public String toString() {
-        return super.toString();
+        return super.toString() + "Voici les réponses proposés : " + Arrays.toString(reponses);
     }
 }
