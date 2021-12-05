d2e0d91906e9eaefa0275de9112e99918c1a544a
Nicolas <xiongnicolas.nx@gmail.com>
2021-12-05 17:49:56 +0100

    dqqs

--- src/questions/Question_QCM.java
+++ src/questions/Question_QCM.java
@@ -1,8 +1,37 @@
 package questions;
+import java.util.Scanner;
 
 public class Question_QCM extends Question {
 
-    public Question_QCM(int d, String description, String type, int num) {
+    private int reponse;
+
+    public Question_QCM(int d, String description, String type, int num, int rep) {
         super(d, description, type, num);
+        this.reponse = rep;
+    }
+
+    public void repondre(){
+        boolean valid = false;
+        while(!valid) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Saisir la bonne réponse à la question");
+            int rep = scanner.nextInt();
+            if ((rep <= 0) || (rep > 3)) {
+                System.out.println("La réponse n'est pas valide");
+            } else {
+                valid = true;
+                this.reponse = rep;
+            }
+        }
+    }
+    public void setReponse(int rep){
+        this.reponse = rep;
+    }
+    public int getReponse(){
+        return this.reponse;
+    }
+    @Override
+    public String toString(){
+        return super.toString() + "\n La bonne réponse de la question : " + this.reponse;
     }
 }
--- src/questions/Question_VF.java
+++ src/questions/Question_VF.java
@@ -1,8 +1,36 @@
 package questions;
+import java.util.Scanner;
 
 public class Question_VF extends Question {
 
-    protected Question_VF(int d, String description, String type, int num) {
+    private String reponse;
+    protected Question_VF(int d, String description, String type, int num, String rep) {
         super(d, description, type, num);
+        this.reponse= rep;
+    }
+    public void repondre(){
+        boolean valid = false;
+        while(!valid) {
+            System.out.println("Saisir la réponse à la question :");
+            Scanner scanner = new Scanner(System.in);
+            String response = scanner.nextLine();
+            if ((response.equals("vrai")) || (response.equals("faux"))) {
+                valid = true;
+                this.reponse = response;
+            } else {
+                System.out.println("La réponse n'est pas valable, choisissez vrai ou faux :");
+            }
+        }
+    }
+
+    public void setReponse(String rep){
+        this.reponse = rep;
+    }
+    public String getReponse(){
+        return this.reponse;
+    }
+    @Override
+    public String toString(){
+        return super.toString() + "\n La bonne réponse de la question : " + this.reponse;
     }
 }
